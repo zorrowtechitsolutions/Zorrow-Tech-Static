75fdfc0f3ade4b32725f6dc2ed1903e669055790
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2025-07-11 22:23:39 +0530

    Change the location

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -52,8 +52,17 @@ const Contact = () => {
 
             {/* Map */}
             <div className="md:col-span-2 h-[300px] overflow-hidden flex justify-end ">
-              <iframe
+              {/* <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d489.4947040551085!2d76.07961950212783!3d11.041803445034844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba64a9f35db59bb%3A0xc7540f7fcdffdc93!2sMalayala%20Manorama%20Circle!5e0!3m2!1sen!2sin!4v1742115226812!5m2!1sen!2sin"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe> */}
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d489.5043813040484!2d76.10250399781484!3d11.035997159470721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1752252529802!5m2!1sen!2sin"
                 width="600"
                 height="450"
                 style={{ border: 0 }}
